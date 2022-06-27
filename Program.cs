f600caaf6145fecfc59ea32aeb88ede49f226ef4
Svetlana <lankaby@yandex.ru>
2022-06-27 17:47:04 +0300

    Добавлен ответ на третью задачу

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 ﻿// Задача 1. // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.Write("Введите трехзначное число ");
+/*Console.Write("Введите трехзначное число ");
 int a = Convert.ToInt32(Console.ReadLine());
 
 int firstDigit = a / 10;
 int secondDigit = firstDigit % 10; 
-Console.WriteLine($"{a} -> {secondDigit}" );
+Console.WriteLine($"{a} -> {secondDigit}" );*/
 
+/* Задача 3
+Console.Write("Введите число (1-7) ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 6 || a == 7)  
+{
+   Console.WriteLine("Выходной");
+}
+
+else
+{
+Console.WriteLine("Будни");
+}
+*/
